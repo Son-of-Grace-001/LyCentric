c3484abd4bef55e2f0e9abbc7d417008021de5b3
olajirestephen <horlharmighty2000@gmail.com>
2025-07-19 23:43:11 -0600

    modified the face

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x-4-gi@^u6rnz4p9!jpa&@bw+9t5mytcl5)dnckcmkmg51q0"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
